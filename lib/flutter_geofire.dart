6075e31714145a30158d31192747f8ea855488c8
Venance Edson <robilin2@yahoo.com>
2023-12-05 00:42:52 +0300

    Update flutter_geofire.dart
    fix null related errors

--- lib/flutter_geofire.dart
+++ lib/flutter_geofire.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class Geofire {
-  static const MethodChannel _channel = const MethodChannel('geofire');
-
-  static const EventChannel _stream = const EventChannel('geofireStream');
+  static const MethodChannel _channel = MethodChannel('geofire');
+  static const EventChannel _stream = EventChannel('geofireStream');
 
   static const onKeyEntered = "onKeyEntered";
   static const onGeoQueryReady = "onGeoQueryReady";
@@ -15,57 +13,44 @@ class Geofire {
   static Stream<dynamic>? _queryAtLocation;
 
   static Future<bool> initialize(String path) async {
-    final dynamic r = await _channel
+    final dynamic result = await _channel
         .invokeMethod('GeoFire.start', <String, dynamic>{"path": path});
-    return r ?? false;
+    return result as bool? ?? false;
   }
 
-  static Future<bool?> setLocation(
+  static Future<bool> setLocation(
       String id, double latitude, double longitude) async {
     final bool? isSet = await _channel.invokeMethod('setLocation',
         <String, dynamic>{"id": id, "lat": latitude, "lng": longitude});
-    return isSet;
+    return isSet ?? false;
   }
 
-  static Future<bool?> removeLocation(String id) async {
+  static Future<bool> removeLocation(String id) async {
     final bool? isSet = await _channel
         .invokeMethod('removeLocation', <String, dynamic>{"id": id});
-    return isSet;
+    return isSet ?? false;
   }
 
-  static Future<bool?> stopListener() async {
+  static Future<bool> stopListener() async {
     final bool? isSet =
         await _channel.invokeMethod('stopListener', <String, dynamic>{});
-    return isSet;
+    return isSet ?? false;
   }
 
   static Future<Map<String, dynamic>> getLocation(String id) async {
-    final Map<dynamic, dynamic> response = await (_channel
-        .invokeMethod('getLocation', <String, dynamic>{"id": id}));
-
-    Map<String, dynamic> location = new Map();
-
-    response.forEach((key, value) {
-      location[key] = value;
-    });
-
-    // print(location);
-
-    return location;
+    final Map<dynamic, dynamic>? response = await (_channel
+        .invokeMethod('getLocation', <String, dynamic>{"id": id})) as Map<dynamic, dynamic>?;
+    return response?.map<String, dynamic>((key, value) => MapEntry(key as String, value)) ?? {};
   }
 
   static Stream<dynamic>? queryAtLocation(
       double lat, double lng, double radius) {
     _channel.invokeMethod('queryAtLocation',
-        {"lat": lat, "lng": lng, "radius": radius}).then((result) {
-      // print("result" + result);
-    }).catchError((error) {
-      // print("Error " + error);
+        {"lat": lat, "lng": lng, "radius": radius}).catchError((error) {
+      // Handle error
     });
 
-    if (_queryAtLocation == null) {
-      _queryAtLocation = _stream.receiveBroadcastStream();
-    }
+    _queryAtLocation ??= _stream.receiveBroadcastStream();
     return _queryAtLocation;
   }
 }
